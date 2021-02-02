97c2d392a44817800fc03d930fc0dfd986798a56
thirdsgames <thirdsgames2018@gmail.com>
2021-02-02 17:36:30 +0000

    Create "Hello, world" LLVM assembly

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use inkwell::execution_engine::{ExecutionEngine, JitFunction};
-use inkwell::module::Module;
-use inkwell::targets::{InitializationConfig, Target};
 use inkwell::OptimizationLevel;
 use inkwell::{
     builder::Builder,
     targets::{CodeModel, RelocMode, TargetMachine, TargetTriple},
 };
+use inkwell::{
+    comdat::Comdat,
+    targets::{InitializationConfig, Target},
+};
 use inkwell::{context::Context, targets::FileType};
-use std::{error::Error, io::BufWriter, path::Path, process::Command};
+use inkwell::{
+    execution_engine::{ExecutionEngine, JitFunction},
+    AddressSpace,
+};
+use inkwell::{
+    module::{Linkage, Module},
+    values::PointerValue,
+};
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+    io::BufWriter,
+    path::Path,
+    process::{Command, Output},
+};
 
 /// Convenience type alias for the `sum` function.
 ///
@@ -42,6 +57,76 @@ impl<'ctx> CodeGen<'ctx> {
 
         unsafe { self.execution_engine.get_function("sum").ok() }
     }
+
+    fn jit_compile_main(&self) -> Option<JitFunction<SumFunc>> {
+        let i8_t = self.context.i8_type();
+        let i32_t = self.context.i32_type();
+        let i64_t = self.context.i64_type();
+
+        let fn_type = i32_t.fn_type(
+            &[
+                i32_t.into(),
+                i8_t.ptr_type(AddressSpace::Generic)
+                    .ptr_type(AddressSpace::Generic)
+                    .into(),
+            ],
+            false,
+        );
+        let function = self.module.add_function("main", fn_type, None);
+        let basic_block = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(basic_block);
+
+        let puts = self.module.add_function(
+            "puts",
+            i32_t.fn_type(&[i8_t.ptr_type(AddressSpace::Generic).into()], false),
+            Some(Linkage::External),
+        );
+        let hello_world_message = unsafe {
+            self.builder
+                .build_global_string("Hello, world!", "hello_world")
+        };
+        hello_world_message.set_linkage(Linkage::LinkOnceODR);
+        //hello_world_message.set_visibility(visibility)
+        hello_world_message.set_constant(true);
+        let hello_world_deref = unsafe {
+            self.builder.build_in_bounds_gep(
+                hello_world_message.as_pointer_value(),
+                &[i64_t.const_int(0, false), i64_t.const_int(0, false)],
+                "hello_world_deref",
+            )
+        };
+        self.builder
+            .build_call(puts, &[hello_world_deref.into()], "call");
+
+        self.builder.build_return(Some(&i32_t.const_int(0, false)));
+
+        unsafe { self.execution_engine.get_function("sum").ok() }
+    }
+}
+
+struct ExecutionError {
+    program: String,
+    output: Output,
+}
+
+impl Error for ExecutionError {}
+
+impl Debug for ExecutionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{} failed with {}", self.program, self.output.status)?;
+        writeln!(f, "Output:")?;
+        writeln!(f, "{}", std::str::from_utf8(&self.output.stdout).unwrap())?;
+        writeln!(f, "Error Output:")?;
+        writeln!(f, "{}", std::str::from_utf8(&self.output.stderr).unwrap())?;
+        Ok(())
+    }
+}
+
+impl Display for ExecutionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(self, f)
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -61,14 +146,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         execution_engine,
     };
 
-    let sum = codegen
-        .jit_compile_sum()
-        .ok_or("Unable to JIT compile `sum`")?;
+    //let sum = codegen
+    //    .jit_compile_sum()
+    //    .ok_or("Unable to JIT compile `sum`")?;
+
+    codegen.jit_compile_sum();
+    codegen.jit_compile_main();
 
     Target::initialize_x86(&InitializationConfig::default());
 
     let opt = OptimizationLevel::Default;
-    let reloc = RelocMode::Default;
+    let reloc = RelocMode::PIC;
     let model = CodeModel::Default;
     let path = Path::new("foo.o");
     let target = Target::from_name("x86-64").unwrap();
@@ -83,6 +171,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         )
         .unwrap();
 
+    // Output the LLVM bitcode, and decompile it if we have `llvm-dis` on the system.
+    codegen
+        .module
+        .write_bitcode_to_path(&path.with_extension("bc"));
+    let _ = Command::new("llvm-dis")
+        .arg(path.with_extension("bc").to_str().unwrap())
+        .status();
+    assert!(target_machine
+        .write_to_file(
+            &codegen.module,
+            FileType::Assembly,
+            &path.with_extension("asm")
+        )
+        .is_ok());
     assert!(target_machine
         .write_to_file(&codegen.module, FileType::Object, &path)
         .is_ok());
@@ -93,20 +195,35 @@ fn main() -> Result<(), Box<dyn Error>> {
         let mut cmakelists = BufWriter::new(std::fs::File::create("CMakeLists.txt").unwrap());
         writeln!(cmakelists, "cmake_minimum_required (VERSION 3.10)").unwrap();
         writeln!(cmakelists, "project (TEST)").unwrap();
-        writeln!(cmakelists, "add_executable (test foo.o ../util.c)").unwrap();
+        writeln!(cmakelists, "add_executable (test foo.o glue.c)").unwrap();
+        writeln!(
+            cmakelists,
+            "set_target_properties (test PROPERTIES LINKER_LANGUAGE C)"
+        )
+        .unwrap();
     }
 
     let cmake_build_folder = format!("cmake_{}", host_triple);
     let _ = std::fs::create_dir(&cmake_build_folder);
     std::env::set_current_dir(&cmake_build_folder).unwrap();
     let cmake_configure = Command::new("cmake").arg("..").output().unwrap();
-    assert!(cmake_configure.status.success());
+    if !cmake_configure.status.success() {
+        return Err(Box::new(ExecutionError {
+            program: "cmake configure".to_owned(),
+            output: cmake_configure,
+        }));
+    }
     let cmake_build = Command::new("cmake")
         .arg("--build")
         .arg(".")
         .output()
         .unwrap();
-    assert!(cmake_build.status.success());
+    if !cmake_build.status.success() {
+        return Err(Box::new(ExecutionError {
+            program: "cmake build".to_owned(),
+            output: cmake_build,
+        }));
+    }
     assert!(run_executable().status().unwrap().success());
 
     // let x = 1u64;
@@ -124,7 +241,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn run_executable() -> Command {
     #[cfg(windows)]
     {
-        Command::new("Debug/test.exe")
+        Command::new("./Debug/test.exe")
     }
     #[cfg(unix)]
     {
