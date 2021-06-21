d9a663d658f66eaba63cbb303a73a9d853467c3b
thirdsgames <thirdsgames2018@gmail.com>
2021-06-21 20:26:32 +0100

    Fully implement recursive type check
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/quill_type_deduce/src/hindley_milner/substitute.rs
+++ src/quill_type_deduce/src/hindley_milner/substitute.rs
@@ -279,7 +279,7 @@ fn typeck_impl(
         messages: Vec::new(),
     };
 
-    typeck.compute_impl(impl_token, aspect, cases, visible_names)
+    typeck.compute_impl(impl_token, aspect, &parameters, cases, visible_names)
 }
 
 fn substitute_type(
