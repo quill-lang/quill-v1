e4ec8c80345594d0c9ecca436b2aebf34f804fc8
thirdsgames <thirdsgames2018@gmail.com>
2021-06-28 18:08:10 +0100

    Create automated tests
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

---
+++ src/quillc/build.rs
@@ -0,0 +1,50 @@
+use std::{
+    io::{BufWriter, Write},
+    path::{Path, PathBuf},
+};
+
+/// Automatically generate the automated test module.
+pub fn main() {
+    println!("cargo:rerun-if-changed=tests");
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let destination = std::path::Path::new(&out_dir).join("tests.rs");
+    let f = std::fs::File::create(&destination).unwrap();
+
+    // Scan for every test.
+    scan_dir(
+        &mut BufWriter::new(f),
+        &PathBuf::from("tests"),
+        &PathBuf::new(),
+    );
+}
+
+fn scan_dir(f: &mut impl Write, root: &Path, suffix: &Path) {
+    // Walk through the directory, adding tests as we go.
+    for entry in std::fs::read_dir(root.join(suffix)).unwrap() {
+        let entry = entry.unwrap();
+        let ty = entry.file_type().unwrap();
+        if ty.is_dir() {
+            let this_suffix = suffix.join(entry.file_name());
+            // Is this directory a quill project?
+            if entry.path().join("quill.toml").is_file() {
+                // Yes. Add a unit test.
+                let directory = this_suffix.to_string_lossy().replace('\\', "/");
+                let name = directory.replace("/", "_");
+                write!(
+                    f,
+                    r#"
+                    #[tokio::test]
+                    async fn {}() {{
+                        run_test("{}").await;
+                    }}
+                    "#,
+                    name, directory
+                )
+                .unwrap();
+            } else {
+                // Recurse to see if this directory contains a quill project.
+                scan_dir(f, root, &this_suffix);
+            }
+        }
+    }
+}
