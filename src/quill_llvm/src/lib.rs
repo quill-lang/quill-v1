ec20c85a7bbe470924e67956e90a5aa1db21f363
thirdsgames <thirdsgames2018@gmail.com>
2021-07-28 12:46:25 +0100

    Separate build folders for each target Fixes #148
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/quill_llvm/src/lib.rs
+++ src/quill_llvm/src/lib.rs
@@ -133,12 +133,17 @@ pub fn build(project_name: &str, mir: &ProjectMIR, build_info: BuildInfo) {
         .builder
         .build_return(Some(&codegen.context.i32_type().const_int(0, false)));
 
-    let object_path = build_info.build_folder.join(path);
-    let asm_path = build_info.build_folder.join(path.with_extension("asm"));
+    let target_folder = build_info
+        .build_folder
+        .join(build_info.target_triple.to_string());
+    let _ = std::fs::create_dir(&target_folder);
+
+    let object_path = target_folder.join(path);
+    let asm_path = target_folder.join(path.with_extension("asm"));
     let bc_path = build_info
         .build_folder
         .join(path.with_extension("basic.bc"));
-    let bc_opt_path = build_info.build_folder.join(path.with_extension("bc"));
+    let bc_opt_path = target_folder.join(path.with_extension("bc"));
     let ll_unverified_path = build_info
         .build_folder
         .join(path.with_extension("unverified.ll"));
