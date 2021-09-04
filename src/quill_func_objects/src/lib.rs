f2a72f45ee9b7dc4b6684751ac5a4d3e5cb33922
thirdsgames <thirdsgames2018@gmail.com>
2021-09-04 17:25:18 +0100

    Curry steps are tracked after analysis
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/quill_func_objects/src/lib.rs
+++ src/quill_func_objects/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use quill_common::{location::Location, name::QualifiedName};
 use quill_mir::{
@@ -6,7 +6,8 @@ use quill_mir::{
     ProjectMIR,
 };
 use quill_monomorphise::{
-    mono_mir::MonomorphisedMIR, monomorphisation::MonomorphisationParameters,
+    mono_mir::MonomorphisedMIR,
+    monomorphisation::{CurryStatus, MonomorphisationParameters},
 };
 
 /// Converts curried functions and partial application of functions into more LLVM-friendly representations.
@@ -29,27 +30,62 @@ pub fn convert_func_objects(project: &mut MonomorphisedMIR) {
         }
     }
 
+    let mut curry_steps_used = BTreeMap::new();
+
     for file in project.files.values_mut() {
         for defs in file.definitions.values_mut() {
-            for (params, def) in defs {
-                convert_def(&mut def.def, &arities);
+            for def in defs.values_mut() {
+                convert_def(&mut def.def, &arities, &mut curry_steps_used);
             }
         }
     }
 
-    // TODO: work out curry steps used for each function call
+    // Update the MIR with the known set of curry steps.
+    for (name, map) in curry_steps_used {
+        let defs = project
+            .files
+            .get_mut(&name.source_file)
+            .unwrap()
+            .definitions
+            .get_mut(&name.name)
+            .unwrap();
+        for (params, curry_statuses) in map {
+            defs.get_mut(&params).unwrap().curry_possibilities = curry_statuses;
+        }
+    }
+    project
+        .files
+        .get_mut(&project.entry_point.source_file)
+        .unwrap()
+        .definitions
+        .get_mut(&project.entry_point.name)
+        .unwrap()
+        .get_mut(&MonomorphisationParameters::new(Vec::new()))
+        .unwrap()
+        .curry_possibilities = {
+        let mut set = BTreeSet::new();
+        set.insert(CurryStatus {
+            curry_steps: Vec::new(),
+            direct: true,
+        });
+        set
+    };
 }
 
 fn convert_def(
     def: &mut DefinitionM,
     arities: &BTreeMap<QualifiedName, BTreeMap<MonomorphisationParameters, u64>>,
+    curry_steps_used: &mut BTreeMap<
+        QualifiedName,
+        BTreeMap<MonomorphisationParameters, BTreeSet<CurryStatus>>,
+    >,
 ) {
     if let DefinitionBodyM::PatternMatch(cfg) = &mut def.body {
         for block in cfg.basic_blocks.values_mut() {
             block.statements = block
                 .statements
                 .drain(..)
-                .map(|stmt| convert_stmt(stmt, arities))
+                .map(|stmt| convert_stmt(stmt, arities, curry_steps_used))
                 .flatten()
                 .collect();
         }
@@ -61,6 +97,10 @@ fn convert_def(
 fn convert_stmt(
     stmt: Statement,
     arities: &BTreeMap<QualifiedName, BTreeMap<MonomorphisationParameters, u64>>,
+    curry_steps_used: &mut BTreeMap<
+        QualifiedName,
+        BTreeMap<MonomorphisationParameters, BTreeSet<CurryStatus>>,
+    >,
 ) -> Vec<Statement> {
     match stmt.kind {
         StatementKind::InstanceSymbol {
@@ -83,6 +123,18 @@ fn convert_stmt(
             if arity == 0 {
                 // If the function is a nullary function, i.e. a (possibly polymorphic) constant, a function with no arguments,
                 // then we can call it right away.
+                curry_steps_used
+                    .entry(name.clone())
+                    .or_default()
+                    .entry(
+                        MonomorphisationParameters::new(type_variables.clone())
+                            .with_args(special_case_arguments.iter().cloned()),
+                    )
+                    .or_default()
+                    .insert(CurryStatus {
+                        curry_steps: Vec::new(),
+                        direct: true,
+                    });
                 vec![Statement {
                     range: stmt.range,
                     kind: StatementKind::InvokeFunction {
@@ -95,6 +147,25 @@ fn convert_stmt(
                 }]
             } else {
                 // We need to create a new "dummy" function that applies one argument to this function.
+                let entry = curry_steps_used
+                    .entry(name.clone())
+                    .or_default()
+                    .entry(
+                        MonomorphisationParameters::new(type_variables.clone())
+                            .with_args(special_case_arguments.iter().cloned()),
+                    )
+                    .or_default();
+                entry.insert(CurryStatus {
+                    curry_steps: std::iter::repeat(1).take(arity as usize).collect(),
+                    direct: true,
+                });
+                for i in 1..=arity {
+                    entry.insert(CurryStatus {
+                        curry_steps: std::iter::repeat(1).take(i as usize).collect(),
+                        direct: false,
+                    });
+                }
+
                 vec![Statement {
                     range: stmt.range,
                     kind: StatementKind::ConstructFunctionObject {
